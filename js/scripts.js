3e65316d43af0711d77f361638d1d7938cd53722
smedina06 <smedina@mail.endicott.edu>
2026-04-16 13:33:06 -0400

    navmenu

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,33 @@
-
-
 //  Yet another way to add the current year to the footer
 document.querySelector("#copyright").textContent = new Date().getFullYear();
+
+
+// mobile menu 
+const navigationMenu = document.getElementById("navmenu");
+const navButton = document.getElementById("toggle-menu");
+
+function toggleMenu() {
+    let isVisble = navigationMenu.getAttribute("data-visible");
+
+    if(isVisble === "false") {
+        if(navigationMenu.classList.contains("active")){
+            navigationMenu.setAttribute("data-visible", "true");
+            navButton.setAttribute("data-visble", "true");
+        }
+        else {
+            dataDisplay();
+            setTimeout(toggleMenu, 200);
+        }
+    }
+    else {
+        navigationMenu.setAttribute("data-visible", "false");
+        navButton.setAttribute("data-visible", "false");
+        setTimeout(dataDisplay, 200);
+    }
+
+    function dataDisplay() {
+        navigationMenu.classList.toggle("active");
+    }
+}
+
+navButton.addEventListener("click", toggleMenu);
